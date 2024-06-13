0a82aafe0837d608d571ac98ff7a219e63caf2f7
wkdu0723 <57204674+wkdu0723@users.noreply.github.com>
2024-06-13 22:57:48 +0900

    [Bronze II] Title: 블랙잭, Time: 176 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -6,24 +6,19 @@ const number = input.shift().split(" ").map(Number);
 const n = number[0];
 const list = input[0].split(" ").map(Number);
 const targetNumber = number[1];
-let targetX = 0;
-let targetY = 0;
-let targetZ = 0;
-const result = [];
+let sum = 0;
+let maxSum = 0;
 
 for (let x = 0; x < n; x++) {
-    targetX = list[x];
     for (let y = x + 1; y < n; y++) {
-        targetY = list[y];
         for (let z = y + 1; z < n; z++) {
-            targetZ = list[z];
-            result.push(targetX + targetY + targetZ);
+            sum = list[x] + list[y] + list[z];
+            if (sum > targetNumber) continue;
+            
+            if (sum === targetNumber) return console.log(sum);
+            else if (sum < targetNumber && sum > maxSum) maxSum = sum;
         }
     }
 }
 
-const maxNumber = result.sort((a, b) => b - a).find((item) => {
-    if (item <= targetNumber) return item;
-});
-
-console.log(maxNumber);
+console.log(maxSum);
